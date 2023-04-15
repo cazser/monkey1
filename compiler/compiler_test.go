ae1689f2e70fe39dfbeed4d187ba3216a525923f
cazser <2264712072@qq.com>
2023-04-16 07:05:04 +0800

    add test code

---
+++ compiler/compiler_test.go
@@ -0,0 +1,118 @@
+package compiler
+
+import (
+	"fmt"
+	"monkey/ast"
+	"monkey/lexer"
+	"monkey/parser"
+	"monkey/code"
+	"monkey/object"
+	"testing"
+)
+
+type compilerTestCase struct{
+	input string
+ expectedConstants []interface{}
+ expectedInstrcutions []code.Instructions
+}
+
+func TestIntegerArithemtic(t *testing.T){
+	tests:= []compilerTestCase{
+		input: "1+2",
+		expectedConstants: []interface{}{1, 2},
+		expectedInstrcutions: []code.Instructions{
+			code.Make(code.OpConstant, 0),
+			code.Make(code.OpConstant, 1),
+		},
+
+	},
+
+	runCompilerTests(t, tests);
+}
+
+
+func runCompilerTests(t *testing.T, tests []compilerTestCase){
+	t.Helper();
+	for _, tt := range tests{
+		program:= parse(tt.input);
+		compiler:= New();
+		err:= compiler.Compile(program);
+		if err!= nil{
+			t.Fatalf("complete error: %s", err);
+		}
+
+		bytecode:= compiler.ByteCode();
+
+		err= testInstructions(tt.expectedInstrcutions, bytecode.Instructions);
+
+		if err!= nil{
+			t.Fatalf("testInstructions failed: %s", err);
+		}
+		err = testConstant(t, tt.expectedConstants, bytecode.Constants);
+
+		if err!= nil{
+			t.Fatalf("testConstants failed: %s", err);
+		}
+	}
+}
+
+
+func parse(input string) *ast.Program{
+	l:= lexer.New(input);
+	p:= parser.New(l);
+	return p.ParseProgram();
+}
+
+
+func testInstructions(expected []code.Instructions, actual code.Instructions)error{
+	concatted := concatInstructions(expected);
+
+	if len(actual) != len(concatted){
+		return fmt.Errorf("wrong instructions length.\nwant=%q\ngot=%q",
+	concatted,actual)
+	}
+
+	for i, ins:= range concatted{
+		if actual[i]!=ins{
+			return fmt.Errorf("wrong instruction at %d.\nwant=%q\ngot=%q",
+		i, concatted, actual);
+		}
+	}
+
+	return nil;
+}
+
+
+func concatInstructions(s []code.Instructions) code.Instructions{
+	out:= code.Instructions{};
+	for _, ins := range s{
+		out = append(out, ins...)
+	}
+
+	return out;
+}
+
+
+func testConstants(
+	t *testing.T,
+	expected []interface{},
+	actual []object.object,
+)error{
+	if len(expected) != len(actual){
+		return fmt.Errorf("wrong number of constants. got=%d, want=%d",
+	len(actual), len(expected));
+	}
+
+	for i, constant := range expected{
+		switch  constant:= constants.(type) {
+		case int:
+			err:= testIntegerObject(int64(constant), actual[i])
+			if err!= nil{
+				return fmt.Errorf("constant %d - testIntegerObject failed: %s",
+			    i, err);
+			}
+		}
+	}
+
+	return nil;
+}
--- compiler/go.mod
+++ compiler/go.mod
@@ -13,7 +13,13 @@ replace monkey/token => ../token
 require (
 	monkey/ast v0.0.0-00010101000000-000000000000
 	monkey/code v0.0.0-00010101000000-000000000000
+	monkey/lexer v0.0.0-00010101000000-000000000000
 	monkey/object v0.0.0-00010101000000-000000000000
+	monkey/parser v0.0.0-00010101000000-000000000000
 )
 
 require monkey/token v0.0.0-00010101000000-000000000000 // indirect
+
+replace monkey/lexer => ../lexer
+
+replace monkey/parser => ../parser
