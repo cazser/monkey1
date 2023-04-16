4feaccb7fc66dcf16f9b109bce10ece80f974c9a
cazser <2264712072@qq.com>
2023-04-16 17:50:49 +0800

    vm test pass

--- vm/vm.go
+++ vm/vm.go
@@ -16,6 +16,8 @@ type VM struct{
 	stack []object.Object
 	sp int 
 }
+var True = &object.Boolean{Value: true};
+var False = &object.Boolean{Value: false};
 
 
 func New(bytecode *compiler.ByteCode) *VM{
@@ -55,6 +57,17 @@ func (vm *VM) Run() error{
 			}
 		case code.OpPop:
 			vm.pop();
+		case code.OpTrue:
+			err:= vm.push(True)
+			if err!=nil{
+				return err 
+			}
+
+		case code.OpFalse:
+			err:= vm.push(False)
+			if err!=nil{
+				return err 
+			}
 		}
 	}
 
