0a711c3cef255fc59d0a52023c4885be9cf12b2f
cazser <2264712072@qq.com>
2023-04-16 09:23:41 +0800

    add vm test code

--- vm/go.mod
+++ vm/go.mod
@@ -14,9 +14,17 @@ replace monkey/token => ../token
 
 require (
 	monkey/ast v0.0.0-00010101000000-000000000000
+	monkey/compiler v0.0.0-00010101000000-000000000000
 	monkey/lexer v0.0.0-00010101000000-000000000000
 	monkey/object v0.0.0-00010101000000-000000000000
 	monkey/parser v0.0.0-00010101000000-000000000000
 )
 
-require monkey/token v0.0.0-00010101000000-000000000000 // indirect
+require (
+	monkey/code v0.0.0-00010101000000-000000000000 // indirect
+	monkey/token v0.0.0-00010101000000-000000000000 // indirect
+)
+
+replace monkey/compiler => ../compiler
+
+replace monkey/code => ../code
--- vm/vm_test.go
+++ vm/vm_test.go
@@ -2,10 +2,12 @@ package vm
 
 import (
 	"fmt"
+	"testing"
 	"monkey/ast"
 	"monkey/lexer"
 	"monkey/object"
 	"monkey/parser"
+	"monkey/compiler"
 )
 func parse(input string) *ast.Program{
 	l:= lexer.New(input);
@@ -25,4 +27,50 @@ func testIntegerObject(expected int64, actual object.Object) error{
 	result.Value, expected)
 	}
 	return nil;
+}
+
+
+type vmTestCase struct{
+	input string
+	expected interface{}
+}
+
+func runVmTests(t *testing.T, tests []vmTestCase){
+	t.Helper();
+
+	for _, tt := range tests{
+		program:= parse(tt.input);
+
+		comp:= compiler.New();
+		err := comp.Compile(program);
+		if err != nil{
+			t.Fatalf("compiler error: %s", err)
+		}
+
+		vm:= New(comp.ByteCode());
+		err = vm.Run();
+		if err!= nil{
+			t.Fatalf("vm error: %s", err)
+		}
+
+		stackElem := vm.StackTop();
+		testExpectedObject(t, tt.expected, stackElem)
+	}
+}
+
+
+func testExpectedObject(
+	t *testing.T,
+	expected interface{},
+	actual object.Object,
+){
+	t.Helper();
+
+	switch expected:= expected.(type){
+	case int:
+		err:= testIntegerObject(int64(expected), actual);
+		if err != nil{
+			t.Errorf("testIntegerObject failed: %s", err)
+		}
+	}
 }
