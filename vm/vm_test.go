862d3232196e23a1cdb242693cc26023e2f8788e
cazser <2264712072@qq.com>
2023-04-18 07:01:12 +0800

    add test code

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -83,7 +83,26 @@ func testExpectedObject(
 		if err != nil{
 			t.Errorf("testStringObject failed: %s", err);
 		}
-	
+  
+	case []int:
+		array, ok:= actual.(*object.Array);
+		if !ok{
+			t.Errorf("object not Array: %T (%+v)", actual, actual)
+			return;
+		}
+
+		if len(array.Elements) != len(expected){
+			t.Errorf("wrong num of elements. want=%d, got=%d",
+		len(expected), len(array.Elements))
+		return;
+		}
+
+		for i, expectedElem := range expected{
+			err := testIntegerObject(int64(expectedElem), array.Elements[i]);
+			if err != nil{
+				t.Errorf("testTntegerObject failed: %s", err)
+			}
+		}
 	case *object.Null:
 		if actual != Null{
 			t.Errorf("object is not Null: %T (%+v)", actual, actual)
@@ -224,4 +243,15 @@ func testStringObject(expected string, actual object.Object) error{
 	}
 
 	return nil;
+}
+
+
+func TestArrayLieral(t *testing.T){
+	tests:= []vmTestCase{
+		{"[]", []int{}},
+		{"[1, 2, 3]", []int{1, 2, 3}},
+		{"[1 + 2, 3 * 4, 5 + 6]", []int{3, 12, 11}},
+	}
+
+	runVmTests(t, tests);
 }
