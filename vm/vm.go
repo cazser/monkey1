e594ff933fd2752a62049a10aa06f0f33f167f20
cazser <2264712072@qq.com>
2023-04-17 11:59:52 +0800

    test pass

--- vm/vm.go
+++ vm/vm.go
@@ -55,6 +55,18 @@ func (vm *VM) Run() error{
 			if err!=nil{
 				return err;
 			}
+		case code.OpJump:
+			pos:= int(code.ReadUint16(vm.instructions[ip+1:]))
+			ip=pos - 1;
+
+		case code.OpJumpNotTruthy:
+			pos:= int(code.ReadUint16(vm.instructions[ip+1:]))
+			ip+=2;
+			condition:= vm.pop();
+			if !isTruthy(condition){
+				ip = pos - 1;
+			}
+
 		case code.OpPop:
 			vm.pop();
 		case code.OpTrue:
@@ -231,4 +243,14 @@ func (vm *VM) executeMinusOperator() error{
 
 	value:= operand.(*object.Integer).Value;
 	return vm.push(&object.Integer{Value: -value})
+}
+
+
+func isTruthy(obj object.Object)bool{
+  switch obj:= obj.(type){
+	case *object.Boolean:
+		return obj.Value
+	default:
+		return true
+	}
 }
