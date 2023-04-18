e7de9ab95e79234a56c3a302a8056f3b19eea897
cazser <2264712072@qq.com>
2023-04-18 08:26:12 +0800

    vm test fail

--- vm/vm.go
+++ vm/vm.go
@@ -78,6 +78,15 @@ func (vm *VM) Run() error{
 				ip = pos - 1;
 			}
    
+		case code.OpIndex:
+			index:= vm.pop();
+			left:= vm.pop();
+
+			err:= vm.executeIndexExpression(left, index)
+			if err!= nil{
+				return err;
+			}
+
 		case code.OpHash:
 			numElements:= int(code.ReadUint16(vm.instructions[ip+1:]))
 			ip+=2;
@@ -357,4 +366,46 @@ func (vm *VM) buildHash(startIndex, endIndex int) (object.Object, error){
 		hashPairs[hashKey.HashKey()] = pair;
 	}
 	return &object.Hash{Pairs: hashPairs}, nil
+}
+
+
+func (vm *VM) executeIndexExpression(left, index object.Object) error{
+	switch{
+	case left.Type() == object.ARRAY_OBJ && index.Type()== object.INTEGER_OBJ:
+		return vm.executeArrayIndex(left, index);
+	case left.Type() == object.HASH_OBJ:
+		return vm.executeHashIndex(left, index);
+	default:
+		return fmt.Errorf("index operator not supported: %s", left.Type());
+	}
+}
+
+
+func (vm *VM) executeArrayIndex(array, index object.Object) error{
+	arrayObject:= array.(*object.Array);
+	i:= index.(*object.Integer).Value;
+	max:= int64(len(arrayObject.Elements) -1);
+
+	if i<0 || i>max{
+		return vm.push(Null);
+	}
+
+	return vm.push(arrayObject.Elements[i]);
+}
+
+
+func (vm *VM) executeHashIndex(hash, index object.Object) error{
+	hashObject := hash.(*object.Hash)
+
+	key, ok := index.(object.Hashable)
+	if !ok{
+		return fmt.Errorf("unusable as hash key: %s", index.Type());
+	}
+
+	pair, ok := hashObject.Pairs[key.HashKey()];
+	if !ok{
+		return vm.push(Null)
+	}
+
+	return vm.push(pair.Value)
 }
